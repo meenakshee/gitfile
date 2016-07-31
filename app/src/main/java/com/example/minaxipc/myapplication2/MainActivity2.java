dcc68b6c31949ab5c21e7bf215673755c124c99a
meenakshee <durby1892@gmail.com>
2016-07-31 05:45:47 -0700

    Initial commit

---
+++ app/src/main/java/com/example/minaxipc/myapplication2/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.minaxipc.myapplication2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("hello");
+        setContentView(R.layout.activity_main2);
+    }
+}
